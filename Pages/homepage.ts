72849a3a8b3305b0ac35b9580dbd79da80ebdbe6
sahil malviya <sahilmalviya8103@gmail.com>
2026-06-02 13:18:11 +0530

    New Added file

---
+++ Pages/homepage.ts
@@ -0,0 +1,32 @@
+import{Page} from '@playwright/test'
+
+
+class LoginPage{
+
+
+  page: Page;
+   constructor(page:Page){
+    this.page = page;
+  }
+
+ tb_username = "//input[@name='user_name']"
+ tb_password = ''
+ loc_btn_click = ''
+
+
+
+
+async login(Userid: string, Password: string ): Promise<void>{
+  
+  await this.page.fill(this.tb_username, Userid)
+}
+
+async setlogin(Userid: string, Password: string ): Promise<void>{
+
+
+
+
+
+}
+
+}
